095ff0e18cce46cbd312c2c510ff50d8b35790d8
Giovani Generali <giovani.generali@gmail.com>
2020-06-09 19:47:31 -0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
